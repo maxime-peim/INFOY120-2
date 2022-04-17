589761bae8e38f13aff282eb6d4e3f6cf5c73cc6
Maxime Peim <maxime.peim@gmail.com>
2022-04-17 17:24:57 +0200

    Cleaned main.py

--- main.py
+++ main.py
@@ -1,50 +1,12 @@
 import argparse
 from pathlib import Path
 
-# trunk-ignore(flake8/F401)
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-
-# trunk-ignore(flake8/F401)
-from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
-
-# trunk-ignore(flake8/F401)
-from sklearn.feature_selection import SelectFromModel
-
-# trunk-ignore(flake8/F401)
-from sklearn.gaussian_process import GaussianProcessClassifier
-
-# trunk-ignore(flake8/F401)
-from sklearn.gaussian_process.kernels import RBF
-
-# trunk-ignore(flake8/F401)
-from sklearn.linear_model import (
-    LogisticRegression,
-    PassiveAggressiveClassifier,
-    Perceptron,
-    RidgeClassifier,
-    SGDClassifier,
-)
-
-# trunk-ignore(flake8/F401)
-from sklearn.naive_bayes import BernoulliNB, ComplementNB, GaussianNB, MultinomialNB
-
-# trunk-ignore(flake8/F401)
-from sklearn.neighbors import KNeighborsClassifier, NearestCentroid
-
-# trunk-ignore(flake8/F401)
-from sklearn.neural_network import MLPClassifier
-
-# trunk-ignore(flake8/F401)
-from sklearn.pipeline import Pipeline
-
-# trunk-ignore(flake8/F401)
-from sklearn.svm import SVC, LinearSVC, OneClassSVM
-
-# trunk-ignore(flake8/F401)
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
+import manipulation as mpl
 import manipulation.utils as utils
-from manipulation import analyse, classify, extract, preprocess
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -64,6 +26,12 @@ if __name__ == "__main__":
     actions = args.actions
     all_actions = "all" in actions
 
+    # we fix the random state of each classifier, so we can reproduce
+    # the exact results for a given set of parameters
+
+    # clf__ -> classifier's parameters
+    # vect__content_tfidf__ -> body content TFIDF vectorizer's parameters
+    # vect__subject_tfidf__ -> subject TFIDF vectorizer's parameters
     classifiers = [
         (
             RandomForestClassifier(random_state=42, n_jobs=-1),
@@ -78,45 +46,37 @@ if __name__ == "__main__":
                 "vect__subject_tfidf__tfidf__max_features": range(30, 130, 10),
             },
         ),
+        (
+            DecisionTreeClassifier(random_state=42),
+            {
+                "clf__max_depth": range(10, 110, 10),
+                "vect__content_tfidf__tfidf__max_features": range(100, 1000, 100),
+                "vect__subject_tfidf__tfidf__max_features": range(10, 110, 10),
+            },
+        ),
+        (
+            SVC(random_state=42),
+            {
+                "clf__kernel": ["linear", "poly", "rbf", "sigmoid", "precomputed"],
+                "clf__degree": range(1, 10),
+                "vect__content_tfidf__tfidf__max_features": range(100, 1000, 100),
+                "vect__subject_tfidf__tfidf__max_features": range(10, 110, 10),
+            },
+        ),
     ]
-    """
-    classifiers = [
-        (DecisionTreeClassifier(), {
-            'clf__max_depth': range(10, 110, 10),
-            'vect__content_tfidf__tfidf__max_features': range(100, 1000, 100),
-            'vect__subject_tfidf__tfidf__max_features': range(10, 110, 10),
-        })
-    ]
-    classifiers = [
-        (OneClassSVM(), {
-            'clf__kernel' : ['rbf'],
-            'clf__gamma' : [0.001, 0.01, 0.1, 1],
-            'clf__nu': [0.001, 0.01, 0.1, 1],
-            'vect__content_tfidf__tfidf__max_features': range(100, 1000, 100),
-            'vect__subject_tfidf__tfidf__max_features': range(10, 110, 10),
-        })
-    ]"""
-    """
-    classifiers = [
-        (LogisticRegression(solver='saga', tol=1e-2, max_iter=200, random_state=0), {
-            "clf__C": np.linspace(0, 2., 100)[1:],
-            "clf__penalty" : ['l2', 'l1'],
-            'vect__content_tfidf__tfidf__max_features': range(100, 1000, 100),
-            'vect__subject_tfidf__tfidf__max_features': range(10, 110, 10),
-        })
-    ]
-    """
 
     if all_actions or "extract" in actions:
-        extract(args.training_folder, force=True)
-        extract(args.testing_folder, force=True)
+        mpl.extract(args.training_folder, force=True)
+        mpl.extract(args.testing_folder, force=True)
 
     if all_actions or "preprocess" in actions:
-        preprocess(args.training_folder, force=True)
-        preprocess(args.testing_folder, force=True)
+        mpl.preprocess(args.training_folder, force=True)
+        mpl.preprocess(args.testing_folder, force=True)
 
     if all_actions or "classify" in actions:
-        classify(classifiers, args.training_folder, args.testing_folder, plot=args.plot)
+        mpl.classify(
+            classifiers, args.training_folder, args.testing_folder, plot=args.plot
+        )
 
     if all_actions or "analyse" in actions:
-        analyse(args.training_folder, args.testing_folder, args.analyse_folder)
+        mpl.analyse(args.training_folder, args.testing_folder, args.analyse_folder)
